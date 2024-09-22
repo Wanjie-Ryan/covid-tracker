fe913094992d7d53617aa0a939c5a3dc159c3f75
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-09-22 09:22:36 +0300

    covid tracker

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,24 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "plugin:prettier/recommended",
+    "prettier"
   ],
+  plugins: ['prettier'],
   parserOptions: {
     parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': 'error', // Treat Prettier issues as ESLint errors
+    'no-trailing-spaces': 'off',
+    'indent': 'off',
+
+    // Disable blank lines after block statements
+    'padded-blocks': 'off',
+
+    // Disable warnings about multiple blank lines
+    'no-multiple-empty-lines': 'off',
   },
 };
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <header class="bg-primary text-white p-3 text-center fw-bold">
+    <div class="fs-5 fs-md-3 fw-bold">Covid-19 Tracker</div>
+
+    <p class="mt-2">
+      API By
+      <a class="text-light" href="https://covid19api.com" target="_blank"
+        >Covid19api.com</a
+      >
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
